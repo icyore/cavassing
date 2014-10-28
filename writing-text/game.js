cd9383634fe3fbc0a3bdcd05295aa61f4a9cb74c
cblc <lhtyz2008@163.com>
2014-10-28 16:53:11 +0800

    animate boxes

--- writing-text/game.js
+++ writing-text/game.js
@@ -2,11 +2,11 @@ var canvas = document.getElementById('game');
 canvas.width = 800;
 canvas.height = 400;
 var context = canvas.getContext('2d');
-context.font = 'italic 50px Georgia sans-serif';
+context.font = 'italic 50px "Open Sans" sans-serif';
 context.fillStyle = '#ee8833';
 context.fillText('Hello', 100, 100);
 
 context.strokeStyle = '#000'
-context.font = '50px sans-serif';
-context.strokeText('hello', 150, 150);
+context.font = '50px 微软雅黑';
+context.strokeText('你好', 150, 150);
 //context.fillStyle();
