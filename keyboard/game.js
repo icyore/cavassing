62d16585491d21e6a21661f1025dda21bfdcee75
cblc <lhtyz2008@163.com>
2014-10-30 14:46:55 +0800

    add box

--- keyboard/game.js
+++ keyboard/game.js
@@ -1,92 +1,104 @@
 var canvas = document.getElementById('game');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = window.innerWidth - 50;
+canvas.height = window.innerHeight - 50;
 var context = canvas.getContext('2d');
+
+var keys = {};
+
+window.addEventListener('keydown', function (e){
+  keys[e.keyCode] = true;
+  e.preventDefault();
+}, false);
+
+window.addEventListener('keyup', function (e) {
+  delete keys[e.keyCode];
+}, false);
+
+var player = new Box({
+  x: 10,
+  y: 10,
+  width: 50,
+  height: 50,
+  color: '#E39B5F',
+  speed: 5
+});
+var bullets = [];
 
 function Box (options) {
   this.x = options.x || 10;
   this.y = options.y || 10;
   this.width = options.width || 100;
   this.height = options.height || 100;
-  this.color = options.color || '#000000'
+  this.speed = options.speed || 5;
+  this.color = options.color || '#000000';
 }
-
-var boxes = [];
-var drawing = false;
-var boxSize = 20;
-var keys = {};
-var player = new Box({
-  x: 10,
-  y: 10,
-  width: 50,
-  height: 50,
-  color: '#f4ead5',
-  speed: 5
-});
-var bullets = [];
 
 function input (player) {
-  if (37 in keys) {
+  if(37 in keys) {
     player.x -= player.speed;
   }
-  if (39 in keys) {
+  if(39 in keys) {
     player.x += player.speed;
   }
-  if (38 in keys) {
+  if(38 in keys) {
     player.y -= player.speed;
   }
-  if (40 in keys) {
+  if(40 in keys) {
     player.y += player.speed;
   }
-
-  if (32 in keys) {    
-    bullets[bullets.length] = new Box({
-      x: player.x + player.width / 2,
-      y: player.y + player.height / 2,
+  for(var x in keys) {
+    bullets.push(new Box({
+      x: player.x + player.width/2,
+      y: player.y + player.height/2,
       width: 4,
       height: 4,
-      color: '#e3dc01',
+      color: randomColor(),
       speed: 10
-    });
+    }));
+    if(bullets.length > 50) {
+      bullets.shift();
+    }
+  }
+  
+}
+
+function boundaries (box) {
+  if(box.x <= 0) {
+    box.x = 0;
+  }
+  if(box.x + box.width >= canvas.width) {
+    box.x = canvas.width - box.width;
+  }
+  if(box.y <= 0) {
+    box.y =0;
+  }
+  if(box.y + box.height >= canvas.height) {
+    box.y = canvas.height - box.height;
   }
 }
 
-document.addEventListener('keydown', function (e) {
-  keys[e.keyCode] = true;
-  e.preventDefault();
-});
-
-document.addEventListener('keyup', function (e) {
-  delete keys[e.keyCode];
-});
-
-document.addEventListener('mousemove', function (e) {
-  if(drawing) {
-    boxes[boxes.length] = new Box({
-      x: e.clientX - randomNumber(-5,5),
-      y: e.clientY - boxSize/2,
-      width: boxSize,
-      height: boxSize,
-      color: '#f4ead5'
-    });
-  }  
-});
-
-function draw () {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
+function drawBox (box) {
+  context.fillStyle = box.color;
+  context.fillRect(box.x, box.y, box.width, box.height);
 }
 
 function update () {
-
+  input(player);
+  boundaries(player);
 }
 
-
+function draw () {
+  context.clearRect(0, 0, canvas.width,canvas.height);
+  drawBox(player);
+  bullets.forEach(function (b) {
+    drawBox(b);
+  });
+}
 
 function loop () {
   update();
   draw();
-  window.requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 loop();
